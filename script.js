adf5ff9c59a3382791b8c150412383837e13be35
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2020-10-30 08:39:18 -0400

    Creates starter files

---
+++ script.js
@@ -0,0 +1,21 @@
+const themes = {
+  light: {
+    title: 'Light',
+    class: 'light',
+    font: 'https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&display=swap'
+  },
+  dark: {
+    title: 'Dark',
+    class: 'dark',
+    font: 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;1,700&display=swap'
+  },
+  pink: {
+    title: 'Pink',
+    class: 'pink',
+    font: 'https://fonts.googleapis.com/css2?family=Henny+Penny&display=swap'
+  }
+}
+
+const $font = document.getElementById('font')
+const $body = document.querySelector('body')
+const $themes = document.getElementById('themes')
