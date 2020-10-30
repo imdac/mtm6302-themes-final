d4052fb432e0ad1d78315c912c61989588f1a96e
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2020-10-30 08:43:16 -0400

    Adds final version

--- script.js
+++ script.js
@@ -19,3 +19,36 @@ const themes = {
 const $font = document.getElementById('font')
 const $body = document.querySelector('body')
 const $themes = document.getElementById('themes')
+
+function setTheme (theme) {
+  $body.className = theme.class
+  $font.href = theme.font
+}
+
+const ls = localStorage.getItem('theme')
+
+if (ls) {
+  setTheme(themes[ls])
+} else {
+  setTheme(themes.light)
+}
+
+const html = []
+
+for (const theme in themes) {
+  if (theme === ls) {
+    html.push(`<option value="${theme}" selected>${themes[theme].title}</option>`)
+  } else {
+    html.push(`<option value="${theme}">${themes[theme].title}</option>`)
+  }
+}
+
+$themes.innerHTML = html.join('')
+
+$themes.addEventListener('change', function () {
+  const theme = themes[$themes.value]
+
+  localStorage.setItem('theme', $themes.value)
+
+  setTheme(theme)
+})
